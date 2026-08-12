66f462a6cfb07c96360acf44195bf5f59a90a7c0
Ayoub Mihoub <ayoubmihoub80@gmail.com>
2026-08-12 01:51:52 +0100

    fix test configuration

--- user-service/src/test/java/com/irrigo/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/irrigo/userservice/UserServiceApplicationTests.java
@@ -1,12 +1,17 @@
 package com.irrigo.userservice;
 
+import com.irrigo.userservice.service.KeycloakService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class UserServiceApplicationTests {
+
+    @MockitoBean
+    private KeycloakService keycloakService;
 
     @Test
     void contextLoads() {
