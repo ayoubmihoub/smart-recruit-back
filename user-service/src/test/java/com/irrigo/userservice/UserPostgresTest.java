8daba0298a02f4076ac8fdaa10b962953b8399b6
Ayoub Mihoub <ayoubmihoub80@gmail.com>
2026-08-13 00:14:13 +0100

    Fix CI context test

---
+++ user-service/src/test/java/com/irrigo/userservice/UserPostgresTest.java
@@ -0,0 +1,14 @@
+package com.irrigo.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("integration")
+class UserPostgresTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
